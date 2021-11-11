eede18f73efec56e7fb1c6e5e23f77e094a29675
javierpart <84803632+javierpart@users.noreply.github.com>
2021-11-11 19:26:47 +0100

    reto dia 3

---
+++ dia3/Main.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static String pedirNombre(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe el nombre y los apellidos de la persona: ");
+        String nombreCompleto= teclado.nextLine();
+        return nombreCompleto;
+    }
+
+    public static String[] separarNombre (String nombreCompleto){
+        return nombreCompleto.split(" ");
+    }
+
+    public static boolean esNombreComun ( String nombre){
+        String[] nombresComunes = {"Juan", "Maria", "Alberto", "Raul", "Lorena", "Nieves", "Javier", "Alfonso", "Luis", "Lucía", "José"};
+        for (String nombresComun : nombresComunes) {
+            if (nombresComun == nombre)
+                return true;
+        }
+        return false;
+    }
+
+    public static void imprimirNombre (String[] nombreSeparado) {
+        switch (nombreSeparado.length) {
+            case 4:
+                System.out.println("Nombre1 = " + nombreSeparado[0] +
+                        ", Nombre2 = " + nombreSeparado[1] +
+                        ", Apellido1 = " + nombreSeparado[2] +
+                        ", Apellido2 = " + nombreSeparado[3]);
+                break;
+            case 3:
+                if (esNombreComun(nombreSeparado[1])){
+                    System.out.println("Nombre1 = " + nombreSeparado[0] +
+                            ", Nombre2 = " + nombreSeparado[1] +
+                            ", Apellido1 = " + nombreSeparado[2]);}
+                else{
+                    System.out.println("Nombre1 = " + nombreSeparado[0] +
+                            ", Apellido1 = " + nombreSeparado[1] +
+                            ", Apellido2 = " + nombreSeparado[2]);}
+                break;
+            case 2:
+                if (esNombreComun(nombreSeparado[1])){
+                    System.out.println("Nombre1 = " + nombreSeparado[0] +
+                            ", Nombre2 = " + nombreSeparado[1]);}
+                else{
+                    System.out.println("Nombre1 = " + nombreSeparado[0] +
+                            ", Apellido1 = " + nombreSeparado[1]);}
+                break;
+            case 1:
+                System.out.println("Nombre1 = " + nombreSeparado[0]);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        String nombreCompleto = pedirNombre();
+        String[] nombreSeparado = separarNombre(nombreCompleto);
+        imprimirNombre(nombreSeparado);
+    }
+}
